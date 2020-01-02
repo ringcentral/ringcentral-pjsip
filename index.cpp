e4f074a8959eed908e98d9f3ab0f84b463a145f5
Tyler Long <tyler4long@gmail.com>
2020-01-02 13:46:39 -0800

    Add the cpp file

---
+++ index.cpp
@@ -0,0 +1,66 @@
+#include <pjsua2.hpp>
+#include <iostream>
+
+using namespace pj;
+
+// Subclass to extend the Account and get notifications etc.
+class MyAccount : public Account {
+public:
+    virtual void onRegState(OnRegStateParam &prm) {
+        AccountInfo ai = getInfo();
+        std::cout << (ai.regIsActive? "*** Register:" : "*** Unregister:")
+                  << " code=" << prm.code << std::endl;
+    }
+    virtual void onIncomingCall(OnIncomingCallParam &iprm)
+    {
+        std::cout << "********" << std::endl;
+        std::cout << "Incoming call" << std::endl;
+        std::cout << "********" << std::endl;
+    }
+};
+
+int main()
+{
+    Endpoint ep;
+
+    ep.libCreate();
+
+    // Initialize endpoint
+    EpConfig ep_cfg;
+    ep.libInit( ep_cfg );
+
+    // Create SIP transport. Error handling sample is shown
+    TransportConfig tcfg;
+    tcfg.port = 5060;
+    try {
+        ep.transportCreate(PJSIP_TRANSPORT_UDP, tcfg);
+    } catch (Error &err) {
+        std::cout << err.info() << std::endl;
+        return 1;
+    }
+
+    // Start the library (worker threads etc)
+    ep.libStart();
+    std::cout << "*** PJSUA2 STARTED ***" << std::endl;
+
+    // Configure an AccountConfig
+    AccountConfig acfg;
+    acfg.idUri = "sip:<User Name>@sip.ringcentral.com";
+    acfg.regConfig.registrarUri = "sip:sip.ringcentral.com";
+    acfg.sipConfig.proxies = { "sip:<Outbound Proxy>" };
+    AuthCredInfo cred("digest", "*", "<Authorization ID>", 0, "<password>");
+    acfg.sipConfig.authCreds.push_back( cred );
+
+    // Create the account
+    MyAccount *acc = new MyAccount;
+    acc->create(acfg);
+
+    // Here we don't have anything else to do..
+    pj_thread_sleep(300000);
+
+    // Delete the account. This will unregister from server
+    delete acc;
+
+    // This will implicitly shutdown the library
+    return 0;
+}
