e9787810fd269815fbdc6b14e5b587ffc603f0eb
Tyler Long <tyler4long@gmail.com>
2020-01-06 13:36:13 -0800

    Answer inbound call

--- index.cpp
+++ index.cpp
@@ -4,25 +4,31 @@
 
 using namespace pj;
 
-class MyAccount : public Account {
+class MyAccount : public Account
+{
 public:
-  virtual void onRegState(OnRegStateParam &params) {
+  virtual void onRegState(OnRegStateParam &params)
+  {
     AccountInfo acountInfo = getInfo();
     std::cout << (acountInfo.regIsActive ? "*** Register:" : "*** Unregister:")
               << " code=" << params.code << " reason=" << params.reason
               << std::endl;
   }
 
-  virtual void onIncomingCall(OnIncomingCallParam &params) {
+  virtual void onIncomingCall(OnIncomingCallParam &params)
+  {
     Call *call = new Call(*this, params.callId);
     CallOpParam callOpParam;
-    callOpParam.statusCode = PJSIP_SC_DECLINE;
-    call->hangup(callOpParam);
-    delete call;
+    callOpParam.statusCode = PJSIP_SC_OK;
+    call->answer(callOpParam);
+    // callOpParam.statusCode = PJSIP_SC_DECLINE;
+    // call->hangup(callOpParam);
+    // delete call;
   }
 };
 
-int main() {
+int main()
+{
   Endpoint endpoint;
   endpoint.libCreate();
   EpConfig epConfig;
@@ -45,3 +51,22 @@ int main() {
   delete myAccount;
   return 0;
 }
+
+class MyCall : public Call
+{
+public:
+  MyCall(Account &acc, int call_id = PJSUA_INVALID_ID)
+      : Call(acc, call_id)
+  {
+  }
+
+  ~MyCall()
+  {
+  }
+
+  // // Notification when call's state has changed.
+  // virtual void onCallState(OnCallStateParam &prm);
+
+  // // Notification when call's media state has changed.
+  // virtual void onCallMediaState(OnCallMediaStateParam &prm);
+};
