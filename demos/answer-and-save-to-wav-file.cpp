3f0326e0fac06fb3738b6446c2c631f4064ebefc
Tyler Long <tyler4long@gmail.com>
2020-01-07 16:05:43 -0800

    Simplify the answer and save demo

--- demos/answer-and-save-to-wav-file.cpp
+++ demos/answer-and-save-to-wav-file.cpp
@@ -1,4 +1,4 @@
-#include "credentials.hpp"
+#include "../credentials.hpp"
 #include <iostream>
 #include <pjsua2.hpp>
 
@@ -18,63 +18,25 @@ public:
   {
   }
 
-  virtual void onCallState(OnCallStateParam &params)
-  {
-    std::cout << std::endl
-              << "onCallState" << std::endl;
-    CallInfo callInfo = getInfo();
-    std::cout << std::endl
-              << callInfo.stateText << std::endl;
-    if (callInfo.stateText == "DISCONNCTD")
-    {
-      // delete &recorder;
-    }
-  }
-
   virtual void onCallMediaState(OnCallMediaStateParam &params)
   {
-    std::cout << std::endl
-              << "000" << std::endl;
     CallInfo callInfo = getInfo();
     for (unsigned i = 0; i < callInfo.media.size(); i++)
     {
-      std::cout << std::endl
-                << "111" << std::endl;
       if (callInfo.media[i].type == PJMEDIA_TYPE_AUDIO && getMedia(i))
       {
-        std::cout << std::endl
-                  << "222" << std::endl;
-        // AudioMedia *audioMedia = (AudioMedia *)getMedia(i);
-        // recorder.createRecorder("/ringcentral-pjsip/temp.wav");
-        // audioMedia->startTransmit(recorder);
+        AudioMedia *audioMedia = (AudioMedia *)getMedia(i);
+        recorder.createRecorder("/ringcentral-pjsip/temp.wav");
+        audioMedia->startTransmit(recorder);
         break;
       }
     }
-  }
-
-  virtual void onDtmfDigit(OnDtmfDigitParam &params)
-  {
-    std::cout << std::endl
-              << params.digit << std::endl;
   }
 };
 
 class MyAccount : public Account
 {
 public:
-  virtual void onRegState(OnRegStateParam &params)
-  {
-    AccountInfo acountInfo = getInfo();
-    std::cout << (acountInfo.regIsActive ? "*** Register:" : "*** Unregister:")
-              << " code=" << params.code << " reason=" << params.reason
-              << std::endl;
-
-    // Make outbound call
-    // Call *call = new MyCall(*this);
-    // CallOpParam prm(true); // Use default call settings
-    // call->makeCall("sip:6508888888", prm);
-  }
-
   virtual void onIncomingCall(OnIncomingCallParam &params)
   {
     Call *call = new MyCall(*this, params.callId);
