7ced8c917fc8b79438ed70733f2c2c644a3e91eb
Tyler Long <tyler4long@gmail.com>
2020-12-15 11:14:13 -0800

    PJSIP 2.10

--- dockerfile
+++ dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu
 LABEL maintainer="Tyler Liu (tyler.liu@ringcentral.com)"
 RUN apt-get update && apt-get install -y wget build-essential pkg-config
-RUN wget https://www.pjsip.org/release/2.9/pjproject-2.9.tar.bz2 && tar -jxvf pjproject-2.9.tar.bz2
-WORKDIR /pjproject-2.9
+RUN wget https://github.com/pjsip/pjproject/archive/2.10.tar.gz && tar -zxvf 2.10.tar.gz
+WORKDIR /pjproject-2.10
 RUN ./configure && make dep && make && make install
 WORKDIR /ringcentral-pjsip
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 g++ -o /tmp/a.out $1 $(pkg-config --cflags --libs libpjproject)
-export LD_LIBRARY_PATH=/pjproject-2.9/pjsip/lib:/pjproject-2.9/pjlib/lib:/pjproject-2.9/pjmedia/lib:/pjproject-2.9/pjlib-util/lib:/pjproject-2.9/pjnath/lib:/pjproject-2.9/third_party/lib 
+export LD_LIBRARY_PATH=/pjproject-2.10/pjsip/lib:/pjproject-2.10/pjlib/lib:/pjproject-2.10/pjmedia/lib:/pjproject-2.10/pjlib-util/lib:/pjproject-2.10/pjnath/lib:/pjproject-2.10/third_party/lib 
 /tmp/a.out
